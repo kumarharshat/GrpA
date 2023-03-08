f0483932bfbdaccd08eb3adb29e77ed983965b56
harshat <kumarharshat@gmail.com>
2023-03-08 18:33:52 -0500

    Remove comments

--- main.py
+++ main.py
@@ -95,15 +95,3 @@ def run_simulation(tconfig, modelconfig = None, verbose = False, seed = None):
 
     # Return the solver history
     return main_solver.History
-
-
-
-# # Comment this when runnign the tuning file
-# with open("trainingconfig.yaml", "r") as stream:
-#     try:
-#         tconfig = yaml.safe_load(stream)
-#         # print(yaml.safe_load(stream))
-#     except yaml.YAMLError as exc:
-#         print(exc)
-#
-# run_simulation(tconfig, verbose = True)
